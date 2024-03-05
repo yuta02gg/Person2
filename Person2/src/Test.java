ad360ad3fbcc1ae48640c684bf9a7db2cc25d24a
Your Name <you@example.com>
2024-03-05 21:56:00 +0900

    21:55

--- Person2/src/Test.java
+++ Person2/src/Test.java
@@ -1,8 +1,29 @@
 
 public class Test {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
+		Person taro=new Person();
+		taro.name="taro";
+		taro.age=18;
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		Person jiro=new Person("jiro",20);
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		
+		
+		Person saburo=new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+		
+		Person zero=new Person(25);
+		System.out.println(zero.name);
+		System.out.println(zero.age);
+		
+		Person hanako=new Person(17,"hanako");
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 
 	}
 
